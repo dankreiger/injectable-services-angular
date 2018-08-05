c5768a4a93624ae623493364d15b3b2629b063a2
Dan Kreiger <dan@dankreiger.com>
2018-08-05 20:15:06 +0200

    what the hell

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -1,13 +1,14 @@
+import { css } from './waggle-waggle-pup'
 export class CounterService {
   private activeCount = 0;
   private inactiveCount = 0;
   logStatusChangeCount(status: string) {
     if (status === 'active') {
       this.activeCount++;
-      console.log(`Changed to active: ${this.activeCount} ${this.activeCount === 1 ? 'time' : 'times'}`);
+      console.log('%c%s', css, `Changed to active: ${this.activeCount} ${this.activeCount === 1 ? 'time' : 'times'}`);
     } else {
       this.inactiveCount++;
-      console.log(`Changed to inactive: ${this.inactiveCount} ${this.inactiveCount === 1 ? 'time' : 'times'}`);
+      console.log('%c%s', css, `Changed to inactive: ${this.inactiveCount} ${this.inactiveCount === 1 ? 'time' : 'times'}`);
     }
   }
 }
